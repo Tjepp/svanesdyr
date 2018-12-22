4af5b195e143d664907ebd954dfac686ab351dc0
Tjepp <t.bj@outlook.dk>
2018-12-22 13:02:51 +0100

    Added assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable global-require */
+
+export const notification = require('./contactMap.png');
+export const foredragAngstAggression = require('./foredragAngstAggression.jpg');
+export const foredragHundensSprog = require('./foredragHundensSprog.jpg');
+export const foredragSlapAfFido = require('./foredragSlapAfFido.jpg');
+
+/* eslint-enable global-require */
