6adb3e749135c95213e269f7c3673890b71cd35e
Thor <t.bj@outlook.dk>
2020-02-17 12:38:28 +0100

    Opdateret hvalpehold

--- src/pages/DogPuppy.js
+++ src/pages/DogPuppy.js
@@ -94,7 +94,7 @@ const DogPuppy = () => (
                 <Td>19. april</Td>
                 <Td>Ons 4. marts</Td>
                 <Td>12. april</Td>
-                <Td><font color="#D60E0E">2 pladser</font></Td>
+                <Td><font color="#D60E0E">1 plads</font></Td>
               </Tr>
               <Tr>
                 <Td>Hvalp2</Td>
@@ -108,7 +108,7 @@ const DogPuppy = () => (
                 <Td>19. april</Td>
                 <Td>Ons 4. marts</Td>
                 <Td>12. april</Td>
-                <Td><font color="#D60E0E">3 pladser</font></Td>
+                <Td><font color="#D60E0E">2 pladser</font></Td>
               </Tr>
               <Tr>
                 <Td>Hvalp3</Td>
