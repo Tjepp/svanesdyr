054d34ea3d8ee7339912e432d661decd63b53e57
Thor <t.bj@outlook.dk>
2020-08-06 14:34:28 +0200

    meta billede

--- src/components/meta/MetaDogPuppy.js
+++ src/components/meta/MetaDogPuppy.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MetaTags from 'react-meta-tags';
+import { PuppyTwo } from '../../assets';
 
 const MetaDogPuppy = ({ title, description, keywords }) => (
   <>
@@ -12,7 +13,7 @@ const MetaDogPuppy = ({ title, description, keywords }) => (
 
       <meta property="og:locale" content="da_DK" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://www.svanesdyr.dk/HoldHvalp2.jpg" />
+      <meta property="og:image" content={PuppyTwo} />
       <meta property="og:site_name" content="Svanes Dyr" />
 
       <meta name="keywords" content={keywords} />
