b9e3eecf4bc3727a6b599a5e437d7ae15c422582
Thor <t.bj@outlook.dk>
2020-08-09 11:59:58 +0200

    bug fix

--- src/components/meta/MetaDogPuppy.js
+++ src/components/meta/MetaDogPuppy.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MetaTags from 'react-meta-tags';
-import { PuppyTwo } from '../../assets';
 
 const MetaDogPuppy = ({ title, description, keywords }) => (
   <>
