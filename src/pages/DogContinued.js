29cfba897ba785a38ca609b8764ccdb3a0698592
Thor <t.bj@outlook.dk>
2020-09-22 15:40:19 +0200

    fyldte hold

--- src/pages/DogContinued.js
+++ src/pages/DogContinued.js
@@ -108,7 +108,7 @@ const DogContinued = () => (
                 <Td>09.00 - 09.45</Td>
                 <Td>25. oktober</Td>
                 <Td>5. december</Td>
-                <Td><font color="#D60E0E">1 plads</font></Td>
+                <Td><font color="#D60E0E">*Venteliste*</font></Td>
               </Tr>
               {/* <Tr>
                 <Td>Fortsætter H</Td>
