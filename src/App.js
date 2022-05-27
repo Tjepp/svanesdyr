c7b171e3f11a1dad79d3e6c46484d8858aa52f4e
Tjepp <t.bj@outlook.dk>
2022-05-27 18:02:43 +0200

    fix easyme

--- src/App.js
+++ src/App.js
@@ -67,42 +67,44 @@ library.add(
   faTimes
 );
 
-const App = () => (
-  <BrowserRouter>
-    <ScrollToTop />
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/dyreadfaerdskonsulent" exact element={<About />} />
-      <Route path="/kattetraening" exact element={<Cat />} />
-      <Route path="/kontakt" exact element={<Contact />} />
-      <Route path="/hundetraening" exact element={<Dog />} />
-      <Route path="/hvalpevisit" exact element={<DogConsultPuppy />} />
-      <Route path="/hundehold" element={<DogCourse />}></Route>
-      <Route path="/hundehold/tusindtak" element={<DogCourseThanks />} />
-      <Route path="/hundehold/hvalpehold" element={<DogPuppy />} />
-      <Route path="/hundehold/hvalpfortsat" element={<DogContinued />} />
-      <Route path="/hundehold/coolhold" element={<DogCool />} />
-      <Route path="/hundehold/fortsatcool" element={<DogCoolContinued />} />
-      <Route path="/hundehold/venligvovse" element={<DogCoolFriend />} />
-      <Route path="/hundehold/indkald" element={<DogRecall />} />
-      <Route path="/hundehold/socialisering" element={<DogSocial />} />
-      <Route path="/hundehold/negleklipkursus" element={<DogNailClip />} />
-      <Route path="/sommerhund" element={<DogSummer />} />
-      <Route path="/hundehold/empowerment" element={<DogEmpower />} />
-      <Route path="/hundehold/noseworkintro" element={<DogNoseWork />} />
-      <Route path="/hestetraening" exact element={<Horse />} />
-      <Route path="/onlinehandelsbetingelser" element={<LawList />} />
-      <Route path="/privatlivspolitik" element={<LawPrivate />} />
-      <Route path="/foredrag" exact element={<Lecture />} />
-      <Route path="/hvalpmedsucces" exact element={<OnlineEbookPuppyFive />} />
-      <Route path="/eboginvestering" exact element={<OnlineEbookThanks />} />
-      <Route path="/udsolgt" exact element={<OnlineSoldOut />} />
-      <Route path="/sommertak" exact element={<OnlineSummerThanks />} />
-      <Route path="/tusindtak" exact element={<OnlineThankPage />} />
-      <Route path="/gaatur" exact element={<OnlineWalk />} />
-      <Route path="/minemetoder" exact element={<MyMethods />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/dyreadfaerdskonsulent" exact element={<About />} />
+        <Route path="/kattetraening" exact element={<Cat />} />
+        <Route path="/kontakt" exact element={<Contact />} />
+        <Route path="/hundetraening" exact element={<Dog />} />
+        <Route path="/hvalpevisit" exact element={<DogConsultPuppy />} />
+        <Route path="/hundehold" element={<DogCourse />}></Route>
+        <Route path="/hundehold/tusindtak" element={<DogCourseThanks />} />
+        <Route path="/hundehold/hvalpehold" element={<DogPuppy />} />
+        <Route path="/hundehold/hvalpfortsat" element={<DogContinued />} />
+        <Route path="/hundehold/coolhold" element={<DogCool />} />
+        <Route path="/hundehold/fortsatcool" element={<DogCoolContinued />} />
+        <Route path="/hundehold/venligvovse" element={<DogCoolFriend />} />
+        <Route path="/hundehold/indkald" element={<DogRecall />} />
+        <Route path="/hundehold/socialisering" element={<DogSocial />} />
+        <Route path="/hundehold/negleklipkursus" element={<DogNailClip />} />
+        <Route path="/sommerhund" element={<DogSummer />} />
+        <Route path="/hundehold/empowerment" element={<DogEmpower />} />
+        <Route path="/hundehold/noseworkintro" element={<DogNoseWork />} />
+        <Route path="/hestetraening" exact element={<Horse />} />
+        <Route path="/onlinehandelsbetingelser" element={<LawList />} />
+        <Route path="/privatlivspolitik" element={<LawPrivate />} />
+        <Route path="/foredrag" exact element={<Lecture />} />
+        <Route path="/hvalpmedsucces" exact element={<OnlineEbookPuppyFive />} />
+        <Route path="/eboginvestering" exact element={<OnlineEbookThanks />} />
+        <Route path="/udsolgt" exact element={<OnlineSoldOut />} />
+        <Route path="/sommertak" exact element={<OnlineSummerThanks />} />
+        <Route path="/tusindtak" exact element={<OnlineThankPage />} />
+        <Route path="/gaatur" exact element={<OnlineWalk />} />
+        <Route path="/minemetoder" exact element={<MyMethods />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/layout/Page.js
+++ src/components/layout/Page.js
@@ -1,22 +1,35 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import CookieConsent from 'react-cookie-consent';
 import Header from '../Header';
 import Menu from '../menu/Menu';
 import FooterBottom from '../FooterBottom';
 import FooterTop from '../FooterTop';
+import { useLocation } from 'react-router-dom';
 
-const Page = ({ children }) => (
-  <Container>
-    <Header />
-    <Menu />
-    {children}
-    <FooterTop />
-    <FooterBottom />
-    <CookieConsent>This website uses cookies to enhance the user experience. </CookieConsent>
-  </Container>
-);
+const Page = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    onRouteChanged();
+  }, [pathname]);
+
+  const onRouteChanged = () => {
+    window.easyMe(document, 'easyme-connect-kMj1RCBSOhIGq2lrpNHy');
+  };
+
+  return (
+    <Container>
+      <Header />
+      <Menu />
+      {children}
+      <FooterTop />
+      <FooterBottom />
+      <CookieConsent>This website uses cookies to enhance the user experience. </CookieConsent>
+    </Container>
+  );
+};
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
