a1facebb2144ddeb77e93220a8dcac1077e0be56
Thor <t.bj@outlook.dk>
2025-09-08 11:50:04 +0200

    legestue

--- src/pages/DogPlay.js
+++ src/pages/DogPlay.js
@@ -133,7 +133,7 @@ const DogPlay = () => {
             <br />
             <Title text="Datoer for legestuer" showHorizontalRuler />
             <TitleColor text="Din investering: 110 kr." />
-            <TitleColor text="Sommerlegestuerne  varer 60 min. og her vil din investering blive: 145 kr." />
+            {/* <TitleColor text="Sommerlegestuerne  varer 60 min. og her vil din investering blive: 145 kr." /> */}
             <Text>i at få en hyggelig stund i positivt samspil med andre hunde!</Text>
             <Text italic>Bemærk betaling er bindende og beløb refunderes ikke.</Text>
             <br />
