7fbad2db8e0d39e38fd8fe0b1b8efedf62aedac5
Thor <t.bj@outlook.dk>
2025-11-07 08:43:35 +0100

    snackshoppens side

--- src/pages/Snackshop.js
+++ src/pages/Snackshop.js
@@ -7,6 +7,7 @@ import {
   TitleCenter,
   Image,
   Row,
+  Text,
 } from '../components';
 import Responsive from '../components/layout/Responsive';
 import { SnackshopCover } from '../assets';
@@ -19,14 +20,19 @@ const Snackshop = () => (
         <Column widthPercentage="100%">
           <Image src={SnackshopCover} />
           <br />
-          <TitleCenter text="Snackshoppen åbner i slutningen af 2024 - nærmere dato annonceres her på siden" />
+          <TitleCenter text="Snackshoppen åbner fredag den 7. november kl. 12.00 - 17.00" />
           <TitleCenter text="På adressen Horsensvej 31, Nim, 8740 Brædstrup" />
+          <Text>
+            Snackshoppen er en lille, intim gårdbutik i udkanten af Nim, med et stort udvalg af naturlige, tørrede snacks og godbidder til din bedste ven
+          </Text>
+          <TitleCenter text="Snackshoppens åbningstider er den første fredag i måneden kl. 12.00 - 17.00" />
         </Column>
       </Row>
 
       <Row>
         <Column widthPercentage="100%">
-          <TitleCenter color="#872f35" text="Vi ses!" />
+          <TitleCenter color="#872f35" text="Siden her er under opbygning og vil i fremtiden vise Snackshoppens udvalg." />
+          <TitleCenter color="#872f35" text="Snackshoppen er en fysisk butik og ikke en webshop" />
         </Column>
       </Row>
 
