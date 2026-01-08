0439f5a98ac3623b75055a9374de03351aac00f5
Thor <t.bj@outlook.dk>
2026-01-08 14:46:54 +0100

    marts annoncering

--- src/pages/DogCool.js
+++ src/pages/DogCool.js
@@ -131,7 +131,7 @@ const DogCool = () => (
           <Title text="Kommende hold:" />
           <EasyMeList src="https://ezme.io/x/x1S/k3zd" />
           <br />
-          <Title color="#872f35" text="Næste holdstart er Februar 2026" />
+          <Title color="#872f35" text="Næste holdstart er Marts 2026" />
           {/* <Title text="Ønsker du en plads? Så smid mig en mail og få fortrinsret til tilmelding:" /> */}
           <Text>
             Oplever du holdene er fyldt op, eller søger du holdstart på et andet tidspunkt skriver
