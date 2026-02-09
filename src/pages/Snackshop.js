13ce1c95dbf00b5bb86981fb1bce48269fd66c90
Thor <t.bj@outlook.dk>
2026-02-09 12:51:12 +0100

    opdateret næste åbningsdag butik

--- src/pages/Snackshop.js
+++ src/pages/Snackshop.js
@@ -21,22 +21,9 @@ const AllDogs = [
   'Eller de ekstra robuste, til de heftige gnavere der savner ekstra lang tyggetid',
 ];
 
-const Allergy = [
-  'Lam',
-  'Kanin',
-  'Hest',
-];
+const Allergy = ['Lam', 'Kanin', 'Hest'];
 
-const Snacks = [
-  'Buffalo',
-  'Okse',
-  'Ged',
-  'Hjort og Rådyr',
-  'Vildsvin',
-  'Gris',
-  'Kamel',
-  'Kylling',
-];
+const Snacks = ['Buffalo', 'Okse', 'Ged', 'Hjort og Rådyr', 'Vildsvin', 'Gris', 'Kamel', 'Kylling'];
 
 const Snackshop = () => (
   <LandingpageOnline>
@@ -47,9 +34,11 @@ const Snackshop = () => (
           <br />
           <TitleCenter text="Snackshoppen kan besøges på adressen Horsensvej 31, Nim, 8740 Brædstrup" />
           <Text>
-            Snackshoppen er en lille, intim gårdbutik i udkanten af Nim, med et stort udvalg af naturlige, tørrede snacks og godbidder til din bedste ven.
+            Snackshoppen er en lille, intim gårdbutik i udkanten af Nim, med et stort udvalg af
+            naturlige, tørrede snacks og godbidder til din bedste ven.
             <br />
-            Du finder over 85 forskellige typer af snacks, hvor du blandt udvalget sagtens kan finde tyggesager der er:
+            Du finder over 85 forskellige typer af snacks, hvor du blandt udvalget sagtens kan finde
+            tyggesager der er:
             <TextListMark texts={AllDogs} />
             Find desuden lige den smagsvariant din hund er fan af eller bedst kan tåle!
             <br />
@@ -57,16 +46,21 @@ const Snackshop = () => (
             <TextList texts={Allergy} />
             Og et større udvalg af:
             <TextList texts={Snacks} />
-            Snackposerne kommer desuden med tydelig EU-mærkat og markering, så du kan se hvor i verden godterne kommer fra og kan shoppe løs med ro i maven.
+            Snackposerne kommer desuden med tydelig EU-mærkat og markering, så du kan se hvor i
+            verden godterne kommer fra og kan shoppe løs med ro i maven.
           </Text>
+          <Text bold>Du kan betale med kontant, mobilepay eller dankort i butikken.</Text>
           {/* <TitleCenter text="Snackshoppens åbningstider er den første fredag i måneden kl. 12.00 - 17.00" /> */}
-          <TitleCenter text="Snackshoppens næste åbningsdag er: fredag den 6. februar 2026 fra kl. 12.00 - 17.00" />
+          <TitleCenter text="Snackshoppens næste åbningsdag er: fredag den 6. marts 2026 fra kl. 12.00 - 17.00" />
         </Column>
       </Row>
 
       <Row>
         <Column widthPercentage="100%">
-          <TitleCenter color="#872f35" text="Siden her er under opbygning og vil i fremtiden vise Snackshoppens udvalg." />
+          <TitleCenter
+            color="#872f35"
+            text="Siden her er under opbygning og vil i fremtiden vise Snackshoppens udvalg."
+          />
           <TitleCenter color="#872f35" text="Snackshoppen er en fysisk butik og ikke en webshop" />
         </Column>
       </Row>
