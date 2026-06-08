ff081cfb4b357704d1312fa48f79e7fde313fb92
Thor <t.bj@outlook.dk>
2026-06-08 11:15:05 +0200

    menu rækkefølge ændret

--- src/components/menu/SummerMenu.js
+++ src/components/menu/SummerMenu.js
@@ -5,15 +5,15 @@ const SummerMenu = () => (
   <MenuContainer>
     <StyledMenuLinkWithIcon link="/sommerhund" text="Praktisk info" />
     <StyledMenuLinkWithIcon link="/sommerhund/aktivering" text="Hverdagsaktivering" />
-    <StyledMenuLinkWithIcon link="/sommerhund/frivillig" text="Frivillig Håndtering" />
     <StyledMenuLinkWithIcon link="/sommerhund/fitness" text="Fitness for alle" />
     <StyledMenuLinkWithIcon link="/sommerhund/hilseroligt" text="Hilse Pænt" />
     <StyledMenuLinkWithIcon link="/sommerhund/nosework" text="Nose Work" />
-    <StyledMenuLinkWithIcon link="/sommerhund/sommerleg" text="Sommerlegestuer" />
+    <StyledMenuLinkWithIcon link="/sommerhund/frivillig" text="Frivillig Håndtering" />
     <StyledMenuLinkWithIcon link="/sommerhund/indkald" text="Indkald" />
     <StyledMenuLinkWithIcon link="/sommerhund/bliv" text="Bliv" />
     <StyledMenuLinkWithIcon link="/sommerhund/tricks" text="Tricks og Sjov" />
     <StyledMenuLinkWithIcon link="/sommerhund/roligeture" text="Rolige Gåture" />
+    <StyledMenuLinkWithIcon link="/sommerhund/sommerleg" text="Sommerlegestuer" />
     <StyledMenuLinkWithIcon link="/" text="Tilbage til Svanes Dyr" />
   </MenuContainer>
 );
